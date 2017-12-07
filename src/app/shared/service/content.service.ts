aa848ae0b23b387053dc670bbbfd481fa5c59432
jmlodge <jm-lodge@hotmail.co.uk>
2017-12-07 18:10:21 +0000

    add style to about

--- src/app/shared/service/content.service.ts
+++ src/app/shared/service/content.service.ts
@@ -13,7 +13,17 @@ export class ContentService {
     },
     'about': {
       title: 'About',
-      subtitle: 'Meet the team!',
+      subtitle: {
+        team: 'Meet the team!',
+        serviceTitle: 'Service',
+        historyTitle: 'History'
+      },
+      service: 'Our company is all about convenience for you.. the customer. We strive to quickly and efficiently repair your\n' +
+      '        broken phone screen at work at home or even using our special recorded delivery. To give you peace of mind\n' +
+      '        that you are getting the best service possible. We only ever use grade A screens sourced from the manufacturers.\n' +
+      '        We are also happy to send you the screen for you to repair yourself.',
+      history: 'Our company is a fictional online smart phone repair service foundered December 2017 as a stream 1 project for\n' +
+      '        code institute diploma in software development. This site is purely aesthetic and does not offer any real services.',
       testTitle: 'Testimonials',
       image: {
         me: 'assets/joseph.jpg',
@@ -21,10 +31,12 @@ export class ContentService {
         testOne: 'assets/test-one.jpg',
         testTwo: 'assets/test-two.jpg',
         testThree: 'assets/test-three.jpg',
+        background: 'assets/dust_scratches.png',
       }
     },
     'service': {
-      image: '/assets/smashed screen.jpg'
+      image: '/assets/smashed screen.jpg',
+      background: '/assets/circles-dark.png'
     },
     'nav': {
       logo: '/assets/mobile-phone-logo-icon.png'
