4a398aa8fb020edd858a787ac913dbb83f5df36d
jmlodge <jm-lodge@hotmail.co.uk>
2017-12-07 12:58:26 +0000

    tidy home css and html adds comments

--- src/app/shared/service/phone-list.ts
+++ src/app/shared/service/phone-list.ts
@@ -1,16 +1,16 @@
 import { PhoneClass} from '../../phone';
 
 export const PHONES: PhoneClass[] = [
-  { id: 1, model: '6', name: 'iPhone', cost: '', screen: '', repair: '', image: 'assets/iphone-6.jpg' },
-  { id: 8, model: '2 XL', name: 'Pixel', cost: '', screen: '', repair: '', image: '/assets/pixel-2-XL.jpg' },
-  { id: 11, model: 'S8 Plus', name: 'Samsung', cost: '', screen: '', repair: '', image: 'assets/samsung-galaxy-s8-plus.jpg' },
-  { id: 9, model: 'U11', name: 'HTC', cost: '', screen: '', repair: '', image: '/assets/htc-u11.jpg' },
-  { id: 2, model: '6s', name: 'iPhone', cost: '', screen: '', repair: '', image: '/assets/iphone-6s-plus.jpg' },
-  { id: 3, model: '7', name: 'iPhone', cost: '', screen: '', repair: '', image: 'assets/iphone-7-plus.jpg' },
-  { id: 4, model: '8', name: 'iPhone', cost: '', screen: '', repair: '', image: 'assets/iPhone-8-plus.jpg' },
-  { id: 5, model: '1', name: 'Pixel', cost: '', screen: '', repair: '', image: 'assets/pixel.jpg' },
-  { id: 6, model: '1 XL', name: 'Pixel', cost: '', screen: '', repair: '', image: 'assets/pixel-xl.jpg' },
-  { id: 7, model: '2', name: 'Pixel', cost: '', screen: '', repair: '', image: 'assets/pixel-2.jpg' },
-  { id: 10, model: 'S8', name: 'Samsung', cost: '', screen: '', repair: '', image: 'assets/samsung-galaxy-s8.jpg' },
-  { id: 12, model: 'S7', name: 'Samsung', cost: '', screen: '', repair: '', image: '/assets/samsung-galaxy-S7.jpg' },
+  { id: 1, model: '6', name: 'iPhone', cost: '£50.00', screen: 'LED 4.7in', repair: '£65.00', image: 'assets/iphone-6.jpg' },
+  { id: 8, model: '2 XL', name: 'Pixel', cost: '£180.00', screen: 'OLED 6in', repair: '£210.00', image: '/assets/pixel-2-XL.jpg' },
+  { id: 11, model: 'S8 Plus', name: 'Samsung', cost: '190.00', screen: 'AMOLED 6.28in', repair: '230.00', image: 'assets/samsung-galaxy-s8-plus.jpg' },
+  { id: 9, model: 'U11', name: 'HTC', cost: '£110.00', screen: 'LCD5 5.5in', repair: '£120.00', image: '/assets/htc-u11.jpg' },
+  { id: 2, model: '6s', name: 'iPhone', cost: '£60.00', screen: 'LED 4.7in', repair: '£75.00', image: '/assets/iphone-6s-plus.jpg' },
+  { id: 3, model: '7', name: 'iPhone', cost: '£70.00', screen: 'LED 4.7in', repair: '£85.00', image: 'assets/iphone-7-plus.jpg' },
+  { id: 4, model: '8', name: 'iPhone', cost: '£80.00', screen: 'LED 4.7in', repair: '£95.00', image: 'assets/iPhone-8-plus.jpg' },
+  { id: 5, model: '1', name: 'Pixel', cost: '£120.00', screen: 'AMOLED 5in', repair: '£150.00', image: 'assets/pixel.jpg' },
+  { id: 6, model: '1 XL', name: 'Pixel', cost: '£160.00', screen: 'AMOLED 5.5in', repair: '£190.00', image: 'assets/pixel-xl.jpg' },
+  { id: 7, model: '2', name: 'Pixel', cost: '£150.00', screen: 'AMOLED 5in', repair: '£180.00', image: 'assets/pixel-2.jpg' },
+  { id: 10, model: 'S8', name: 'Samsung', cost: '£180.00', screen: 'AMOLED 5.8in', repair: '£210.00', image: 'assets/samsung-galaxy-s8.jpg' },
+  { id: 12, model: 'S7', name: 'Samsung', cost: '£120.00', screen: 'OLED 5.1in', repair: '£150.00', image: '/assets/samsung-galaxy-S7.jpg' },
 ];
